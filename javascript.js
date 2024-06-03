aaf5cbdebe9ef0554333a2e6994dea11cfe89339
DA - MainPC <dillion1991@hotmail.com>
2024-06-03 01:23:46 -0600

    Late night. Added playgame() run it. Need to add score counter

--- javascript.js
+++ javascript.js
@@ -8,7 +8,6 @@ function compChoice(options) {
     if (Array.isArray(options)) {
         return options[Math.floor(Math.random() * options.length)]
     }
-
 }
 
 // let comp = compChoice(choices)
@@ -19,65 +18,78 @@ function compChoice(options) {
 function userChoice (options) {
     let user = prompt("Rock, Paper, Scissors: ")
     if (!options.includes(user.toLowerCase())) {
-        console.log("Error: uoe") 
-        return 'uoe'
+        console.log("Error: uoe");
+        return 'uoe';
         // console.log('You must select rock, paper, or scissors')
         
     } else {
-        return user.toLowerCase()
+        return user.toLowerCase();
     }
 }
 
-let userAnswer = userChoice(choices)
-
-
-// Compare Options
-
-function compareOptions(player, computer) {
+// Compare 
+function compareChoices(player, computer) {
     let p = player
     let c = computer
     switch (p) {
         case 'rock':
             switch (c) {
                 case 'paper':
-                    console.log('You lose.');
-                    break;
+                    return 'lose';
                 case 'rock':
-                    console.log('Tie.');
-                    break;
+                    return 'tie';
                 case 'scissors':
-                    break;
+                    return 'win';
             }
         case 'paper':
             switch (c) {
                 case 'scissors':
-                    console.log('You lose.');
-                    break;
+                    return 'lose';
                 case 'paper':
-                    console.log('Tie');
-                    break;
+                    return 'tie';
                 case 'rock':
-                    console.log('You win.');
-                    break;
+                    return 'win';
             }
         case 'scissors':
             switch (c) {
                 case 'rock':
-                    console.log('You lose')
-                    break;
+                    return 'lose';
                 case 'scissors':
-                    console.log('Tie.')
-                    break
+                    return 'tie'
                 case 'paper':
-                    console.log('You win.')
-                    break
+                    return 'win';
             }
-        
+    }
 }
 
 
-
-
-
-
 // Display Results
+
+
+
+// PLAY GAME
+function playGame() {
+    let userScore = Number(0);
+    let compScore = Number(0);
+    let rounds = Number(3);
+    let i = Number(0);
+    while (i < rounds) {
+        let p = userChoice(choices);
+        let c = compChoice(choices);
+        let result = compareChoices(p, c)
+            if (result === 'tie') {
+                console.log("It is a tie.");
+            } else if (result === 'lose') {
+                console.log("You lose this round. Computer +1");
+                compScore++;
+            } else if (result === "win") {
+                console.log("You win this round. +1");
+                userScore++;
+            }
+        i++
+    }
+
+}
+
+playGame()
+
